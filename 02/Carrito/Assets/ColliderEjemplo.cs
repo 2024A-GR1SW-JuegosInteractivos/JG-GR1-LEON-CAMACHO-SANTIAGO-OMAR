9602e306e509a94040bd61e66bcfbc67124f7217
Santiago León <sleoncamacho@outlook.com>
2024-06-20 10:53:45 -0500

    Added sprite images, camara movement and scripts

--- 02/Carrito/Assets/ColliderEjemplo.cs
+++ 02/Carrito/Assets/ColliderEjemplo.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class ColliderEjemplo : MonoBehaviour
 {
-    void OnCollisionEnter2D(UnityEngine.Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("PUM!");
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Entrando en trigger!");
+        if (other.tag == "Paquete")
+        {
+            Debug.Log("Recogio paquete!");
+        }
+        if (other.tag == "Cliente")
+        {
+            Debug.Log("Dejo paquete!");
+        }
     }
 }
