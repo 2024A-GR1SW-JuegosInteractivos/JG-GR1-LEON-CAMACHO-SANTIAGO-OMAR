3e45dd768c6d77297f94dd00d0c6d6e8f302c99e
Santiago León <sleoncamacho@outlook.com>
2024-06-30 21:39:44 -0500

    Created all core gameplay

--- Examen 01/Assets/CarroPlayer.cs
+++ Examen 01/Assets/CarroPlayer.cs
@@ -4,25 +4,49 @@ using UnityEngine;
 
 public class CarroPlayer : MonoBehaviour
 {
-    
     [SerializeField] float steerSpeed = 150.0f;
     [SerializeField] float moveSpeed = 8.0f;
-    
+
     [SerializeField] private float velocidadRapido = 20f;
     [SerializeField] private float velocidadLento = 5f;
-    
-    // Start is called before the first frame update
+
+    private float velocidadOriginal;
+    [SerializeField] private float duracionBoost = 3f;  // Duración del boost
+
     void Start()
     {
-        
+        velocidadOriginal = moveSpeed;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Rapido")
+        {
+            Debug.Log("Ir rapido");
+            StopAllCoroutines();  // Detiene cualquier boost/lento activo
+            StartCoroutine(CambiarVelocidadTemporalmente(velocidadRapido));
+        }
+
+        if (other.tag == "Lento")
+        {
+            Debug.Log("Ir lento");
+            StopAllCoroutines();  // Detiene cualquier boost/lento activo
+            StartCoroutine(CambiarVelocidadTemporalmente(velocidadLento));
+        }
+    }
+
+    IEnumerator CambiarVelocidadTemporalmente(float nuevaVelocidad)
+    {
+        moveSpeed = nuevaVelocidad;
+        yield return new WaitForSeconds(duracionBoost);
+        moveSpeed = velocidadOriginal;
     }
 }
---
+++ Examen 01/Assets/GameBehaviour.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameBehaviour : MonoBehaviour
+{
+    [SerializeField] private float delayDestroy = 0.5f;
+    private bool tieneGF;
+    public GameObject novia; // Asigna la novia en el inspector
+
+    void Start()
+    {
+        novia.SetActive(false); // Asegúrate de que la novia esté desactivada al inicio
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("PUM!");
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Entrando en trigger!");
+        
+        if (other.CompareTag("Enemigo"))
+        {
+            tieneGF = false;
+            Debug.Log("Mataste al enemigo!");
+
+            // Hacer aparecer a la novia
+            novia.SetActive(true);
+
+            // Mover al enemigo y luego destruirlo
+            Rigidbody2D enemyRb = other.GetComponent<Rigidbody2D>();
+            if (enemyRb != null)
+            {
+                Vector2 forceDirection = other.transform.position - transform.position;
+                forceDirection.Normalize();
+                enemyRb.AddForce(forceDirection * 10f, ForceMode2D.Impulse); // Puedes ajustar la fuerza según sea necesario
+                StartCoroutine(DestroyEnemyAfterDelay(other.gameObject));
+            }
+            else
+            {
+                // Si no tiene Rigidbody2D, simplemente destrúyelo después de un retraso
+                Destroy(other.gameObject, delayDestroy);
+            }
+        }
+
+        if (other.CompareTag("GF"))
+        {
+            tieneGF = true;
+            novia.SetActive(false);
+            Debug.Log("Recogiste a tu novia!");
+        }
+
+        if (other.CompareTag("Casa") && tieneGF)
+        {
+            tieneGF = false;
+            Debug.Log("En casa!");
+            // Aquí puedes añadir la lógica para ganar el juego
+        }
+    }
+
+    IEnumerator DestroyEnemyAfterDelay(GameObject enemy)
+    {
+        yield return new WaitForSeconds(delayDestroy);
+        Destroy(enemy);
+    }
+}
